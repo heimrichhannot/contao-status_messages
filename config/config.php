53efbfd12f939ce0e4c4eaff60d9467d61b7eb4d
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-11-22 16:34:29 +0100

    added TL_COMPONENT entry

--- config/config.php
+++ config/config.php
@@ -11,3 +11,9 @@ $GLOBALS['FE_MOD']['miscellaneous']['status_messages'] = 'HeimrichHannot\StatusM
 if (TL_MODE == 'FE') {
 	$GLOBALS['TL_JAVASCRIPT']['jquery.statusmessages'] = 'system/modules/status_messages/assets/js/jquery.statusmessages.js|static';
 }
+
+$GLOBALS['TL_COMPONENTS']['jquery.statusmessages'] = [
+    'js'  => [
+        'system/modules/status_messages/assets/js/jquery.statusmessages.js|static',
+    ],
+];
